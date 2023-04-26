c145a30fb4edcc87f2f8f05df49d67e85a4f4bb4
chrisjacob-s <83893959+chrisjacob-s@users.noreply.github.com>
2023-04-26 02:37:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# Christopher Soriano and Justin Donate
+# 4/25/23
+#
+
+import check_input
+
+def main_menu():
+  '''Displays the main menu and returns the user’s valid input'''
+  print("1. Display current task")
+  print("2. Mark current task complete")
+  print("3. Postpone current task")
+  print("4. Add new task")
+  print("5. Save and quit")
+
+  menu_selection = check_input.get_int_range("Enter choice: ", 1, 5)
+  
+  return menu_selection
+
+def get_date():
+  '''Prompts the user to enter the year, month, and day and returns the date in the format YYYY/MM/DD'''
+  year = check_input.get_int_range('Enter year: ' ,2000,3000)
+  month = check_input.get_int_range('Enter month: ', 1, 12)
+  day = check_input.get_int_range('Enter day: ', 1, 31)
+  
+  return f"{year}/{month:02d}/{day:02d}"
+ 
+def get_time():
+  '''Prompts the user to enter the hour (military time) and minute and returns the time in the format HH:MM'''
+  hour = check_input.get_int_range('Enter hour: ', 0,23)
+  minute = check_input.get_int_range('Enter minute: ',0,59)
+  
+  return f"{hour:02d}:{minute:02d}"
+
+def main():
+  
+  print("-Tasklist-")
+
+main()
---
+++ task.py
@@ -0,0 +1,49 @@
+# Chris
+import abc
+
+class Task(abc.ABC):
+  '''Represents a task that needs to be done on a specific day and time
+  Attributes: 
+    description (str): description of task
+    date (str): due date of task; formatted YYYY/MM/DD
+    time (str): time task is due; formatted HH:MM
+  '''
+
+  def __init__(self, desc, date, time):
+    '''Sets the task's description, date, and time'''
+    self._desc = desc
+    self._date = date
+    self._time = time
+
+  @property
+  def description(self):
+    '''Returns the task's description''' 
+    return self._desc
+
+  def __str__(self): 
+    '''Returns description with the date and time'''
+    return str(self._desc) + " - Due: " + str(self._date) + " at " + str(self._time)
+
+  def __repr__(self):
+    '''Returns a string used to write the task to the file'''
+    return f'{self._desc},{self._date},{self._time}'
+
+  def __lt__(self, other):
+    '''Returns true if the self task is less than the other task'''
+    # Checks if the years, dates, and times are equivalent; if they are then check if the self._desc is less than the other task. If they aren't check which is less than which
+    if self._date[0:4] == other._date[0:4]:
+      if self._date[5:7] == other._date[5:7]:
+        if self._date[8:10] == other._date[8:10]:
+          if self._time[0:2] == other._time[0:2]:
+            if self._time[3:5] == other._time[3:5]:
+              return self._desc < other._desc
+            else: 
+              return self._time[3:5] < other._time[3:5]
+          else: 
+            return self._time[0:2] < other._time[0:2]
+        else: 
+          return self._date[8:10] < other._date[8:10]
+      else:
+        return self._date[5:7] < other._date[5:7]
+    else:
+      return self._date[0:4] < other._date[0:4]
